decef0241b11f6e03c26bf6ce07447bd2b38bd5a
Chloe Blankers <chloeblankers@hotmail.com>
2024-01-14 14:51:25 -0800

    created single todo list

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,7 +1,17 @@
-
+import TodoItem from "./TodoItem";
+import "./styles.css";
 
 function App() {
-  return <p>Isabella and Chloe TEST</p>;
+  const numTodoItems = 10;
+
+  return (
+    <div>
+      <h1 className="font-style">TODO LIST</h1>
+      {Array.from({ length: numTodoItems }, (_, index) => (
+        <TodoItem index={index} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
