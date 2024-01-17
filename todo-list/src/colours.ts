5ca0342755054399eb8103f86e153510d651e915
Chloe Blankers <chloeblankers@hotmail.com>
2024-01-16 20:03:45 -0800

    added selected accent colour

--- todo-list/src/colours.ts
+++ todo-list/src/colours.ts
@@ -5,3 +5,17 @@ export const COLOURS: {
   Black: "#2f3537",
   White: "white",
 };
+
+export const ACCENT_COLOURS: {
+  Blue: string;
+  Purple: string;
+  Green: string;
+  Pink: string;
+  Turquoise: string;
+} = {
+  Blue: "#87b1f5",
+  Purple: "#b587f5",
+  Green: "#b8f587",
+  Pink: "#f5a4da",
+  Turquoise: "#a4f5da",
+};
