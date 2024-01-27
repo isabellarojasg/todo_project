5f98c28ed8cf6b5b293c8d2a330eaf009a3429e2
Isabella Rojas <isabella.rojas.gonzalez@gmail.com>
2024-01-27 14:13:54 -0800

    adding arrows for next and prev week

--- todo-list/src/WeeklyTodoList.js
+++ todo-list/src/WeeklyTodoList.js
@@ -4,16 +4,24 @@ import PropTypes from "prop-types";
 import { ACCENT_COLOURS, COLOURS } from "./colours";
 
 const WeeklyTodoList = (props) => {
-  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
   const currentDate = new Date();
 
-// Get the current day of the week as a number (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
+  // Get the current day of the week as a number (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
   const currentDayNumber = currentDate.getDay();
 
   // Determine the index of the starting day
   const startingDayIndex = daysOfWeek.indexOf(currentDayNumber);
-  //const [currentWeekStartingDay, setCurrentWeekStartingDay] = React.useState(currentDayNumber);
-
+  const [currentWeekStartingDay, setCurrentWeekStartingDay] =
+    React.useState(currentDayNumber);
 
   const todoItemStyles = {
     color: props.isDarkMode ? COLOURS.White : COLOURS.Black,
@@ -21,7 +29,8 @@ const WeeklyTodoList = (props) => {
   };
 
   const goToPreviousWeek = () => {
-    const previousWeekStartingDayIndex = (startingDayIndex - 1 + daysOfWeek.length) % daysOfWeek.length;
+    const previousWeekStartingDayIndex =
+      (startingDayIndex - 1 + daysOfWeek.length) % daysOfWeek.length;
     setCurrentWeekStartingDay(daysOfWeek[previousWeekStartingDayIndex]);
   };
 
@@ -31,20 +40,21 @@ const WeeklyTodoList = (props) => {
   };
 
   return (
-    <div>
-    <button onClick={goToPreviousWeek}>Previous Week</button>
-    <h2>{`Week starting from ${currentDate.toLocaleDateString("en-US", {
-      month: "long",
-      day: "numeric",
-    })}`}</h2>
-    <button onClick={goToNextWeek}>Next Week</button>
-    <div className="weekly-todo-list" style={todoItemStyles}>
-      {daysOfWeek.map((day) => (
-        <DayList key={day} day={day} {...props} />
-      ))}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <i className="fa-solid fa-arrow-left" onClick={goToPreviousWeek}></i>
+      <div className="weekly-todo-list" style={todoItemStyles}>
+        {daysOfWeek.map((day) => (
+          <DayList key={day} day={day} {...props} />
+        ))}
+      </div>
+        <i className="fa-solid fa-arrow-right" onClick={goToNextWeek}></i>
     </div>
-    </div>
-
   );
 };
 
